3906a894302b384d0d4d7e946fab3e5838548b5b
Rootkitcode <coaldar85@gmail.com>
2022-05-23 08:48:40 -0500

    curso java ejercicios practica

---
+++ EjerciciosJava/src/EjerciciosJava/HelloWorld.java
@@ -0,0 +1,17 @@
+//name of package
+package EjerciciosJava;
+
+//class
+public class HelloWorld {
+	
+	//method
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Darwin";
+		System.out.println(name + " Wellcome to java course");
+
+	}
+
+}
