aa0328bc6a4b7bac090842fdb573db7e8515f540
Rootkitcode <coaldar85@gmail.com>
2022-06-07 07:54:31 -0500

    ejercicio crud con springboot y angular

---
+++ gestionEmpleadosBackend/src/main/java/com/gestion/empleados/modelo/Empleado.java
@@ -0,0 +1,70 @@
+package com.gestion.empleados.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
+
+	@Column(name = "apellidos", nullable = false, length = 60)
+	private String apellidos;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	public Empleado() {
+
+	}
+
+	public Empleado(Long id, String nombre, String apellidos, String email) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
