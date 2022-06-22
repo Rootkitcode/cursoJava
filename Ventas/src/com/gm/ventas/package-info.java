ba7fae7ff1201cc300441a797dd68c4afa88b464
Rootkitcode <coaldar85@gmail.com>
2022-06-22 07:54:21 -0500

    ejercicio clases modulo ventas

---
+++ Ventas/src/com/gm/ventas/package-info.java
@@ -0,0 +1,1 @@
+package com.gm.ventas;
---
+++ Ventas/src/com/gm/ventas/test/VentasTest.java
@@ -0,0 +1,21 @@
+package com.gm.ventas.test;
+
+import com.gm.ventas.*;
+
+public class VentasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Producto producto1 = new Producto("Camisa", 50.00);
+		Producto producto2 = new Producto("Pantalon", 100.00);
+		
+		Orden orden1 = new Orden();
+		orden1.agregarProducto(producto1);
+		orden1.agregarProducto(producto2);
+		orden1.mostrarOrden();
+		
+
+	}
+
+}
