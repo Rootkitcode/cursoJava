0249a0ef971c77e4889bb52ce496fd149f226c65
Rootkitcode <coaldar85@gmail.com>
2022-07-14 08:27:55 -0500

    sobreescritura ejercicio java

---
+++ SobreEscritura/src/test/TestEmpleado.java
@@ -0,0 +1,14 @@
+package test;
+
+import domain.Gerente;
+
+public class TestEmpleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gerente gerente1 = new Gerente("Darwin ", 50000, " Sistemas");
+		System.out.println("Gerente: " + gerente1.obtenerDetalles());
+		
+	}
+
+}
